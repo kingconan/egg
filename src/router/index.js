68c051401cf0d5806403078b213574a1cc5ad56c
jingang <jingang@117go.com>
2018-06-28 18:09:08 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import KeyPointCreator from '@/components/KeyPointCreator'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/kpc',
+      name: 'kpc',
+      component: KeyPointCreator
+    },
+    {
+      path: '/',
+      name: 'kpc',
+      component: KeyPointCreator
     }
   ]
 })
